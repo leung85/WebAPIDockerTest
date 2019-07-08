6aa7a72c348baba2badba95ccb48baa15c9828ff
mwkldeveloper <mwkldeveloper@gmail.com>
2019-07-08 14:22:44 +0800

    add  serialport sample

--- WebAppCore3/Controllers/ValuesController.cs
+++ WebAppCore3/Controllers/ValuesController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,6 +23,26 @@ namespace WebAppCore3.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
+            // Get a list of serial port names.             
+            string[] ports = SerialPort.GetPortNames();
+            Console.WriteLine("The following serial ports were found:");
+            // Display each port name to the console.             
+            foreach (string port in ports)             {
+                Console.WriteLine(port);
+            }
+
+
+            using (var led = new SerialPort("/dev/ttyUSB0", 19200, Parity.None, 8, StopBits.One))
+            {
+                led.Open();
+                byte[] start = new byte[] { 0xA5, 0xFF };
+                byte[] text = Encoding.UTF8.GetBytes("1984");
+                byte[] end = new byte[] { 0x00 };
+                byte[] rv = start.Concat(text).Concat(end).ToArray();
+                led.Write(rv, 0, rv.Length);
+            }
+           
+
             return "value";
         }
 
